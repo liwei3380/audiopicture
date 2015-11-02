8c98e9ee2508bf34018c46911ff023ef8b52263f
zlw-802 <zlw-802@zlw.com>
2015-11-02 20:07:25 +0800

    菜单

--- src/com/audiopicture/app/activity/MainActivity.java
+++ src/com/audiopicture/app/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.audiopicture.app.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,6 +21,31 @@ public class MainActivity extends Activity implements OnClickListener {
 		newPro.setOnClickListener(this);
 		editPro.setOnClickListener(this);
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()) {
+		case R.id.works_show:
+			Toast.makeText(MainActivity.this, "works_show", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.new_works:
+			Toast.makeText(MainActivity.this, "new_works", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.system_setting:
+			Toast.makeText(MainActivity.this, "system_setting", Toast.LENGTH_SHORT).show();
+			break;
+		default:
+			break;
+		}
+		return true;
+	}
+	
 	@Override
 	public void onClick(View v){
 		switch (v.getId()) {
